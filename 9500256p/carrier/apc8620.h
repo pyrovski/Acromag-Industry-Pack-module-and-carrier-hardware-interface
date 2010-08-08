3e3842e55de55813d7abf603f3874c6b5a365337
Peter Bailey <pbailey@lbti.org>
2010-08-08 14:27:20 -0700

    formatting

--- 9500256p/carrier/apc8620.h
+++ 9500256p/carrier/apc8620.h
@@ -1,67 +1,67 @@
 
 /*
-{+D}
-    SYSTEM:			Linux
+  {+D}
+  SYSTEM:			Linux
 
-    FILENAME:	    APC8620.h
+  FILENAME:	    APC8620.h
 
-    MODULE NAME:    Functions common to the APC8620 example software.
+  MODULE NAME:    Functions common to the APC8620 example software.
 
-    VERSION:	    H
+  VERSION:	    H
 
-    CREATION DATE:  06/06/01
+  CREATION DATE:  06/06/01
 
-    DESIGNED BY:    FJM
+  DESIGNED BY:    FJM
 
-    CODED BY:	    FJM
+  CODED BY:	    FJM
 
-    ABSTRACT:       This file contains the definitions, structures
-                    and prototypes for APC8620A.
-                    The APC8620A is a enhanced version of the APC8620.
-                    This library may be used with both enhanced (APC8620A)
-                    and non-enhanced (APC8620) carriers.
-                    Calling the enhanced functions will not harm existing
-                    non-enhanced (APC8620) hardware.
-                    The enhanced functions check to be sure of the carrier's
-                    capabilities.
-                    CarrierInitialize() must be called before calling any
-                    enhanced function.
-                    Enhanced functions will return "E_NOT_IMPLEMENTED" for
-                    a non-enhanced (APC8620) carrier.
+  ABSTRACT:       This file contains the definitions, structures
+  and prototypes for APC8620A.
+  The APC8620A is a enhanced version of the APC8620.
+  This library may be used with both enhanced (APC8620A)
+  and non-enhanced (APC8620) carriers.
+  Calling the enhanced functions will not harm existing
+  non-enhanced (APC8620) hardware.
+  The enhanced functions check to be sure of the carrier's
+  capabilities.
+  CarrierInitialize() must be called before calling any
+  enhanced function.
+  Enhanced functions will return "E_NOT_IMPLEMENTED" for
+  a non-enhanced (APC8620) carrier.
 
-    CALLING
-	SEQUENCE:
+  CALLING
+  SEQUENCE:
 
-    MODULE TYPE:
+  MODULE TYPE:
 
-    I/O RESOURCES:
+  I/O RESOURCES:
 
-    SYSTEM
-	RESOURCES:
+  SYSTEM
+  RESOURCES:
 
-    MODULES
-	CALLED:
+  MODULES
+  CALLED:
 
-    REVISIONS:
+  REVISIONS:
 
   DATE	  BY	    PURPOSE
--------  ----	------------------------------------------------
-12/22/04 FJM    Fedora core FC3 update.
-01/20/05 FJM    Extended carrier function library interface.
-09/27/05 FJM    Fedora core FC4 update.
-12/06/06 FJM    Removed handler_data from IP.h files - Moved handler_data here
-01/11/07 FJM    Add support x86_64
-04/01/09 FJM    blocking_start_convert
-08/02/09 FJM    Add configuration register access.
+  -------  ----	------------------------------------------------
+  12/22/04 FJM    Fedora core FC3 update.
+  01/20/05 FJM    Extended carrier function library interface.
+  09/27/05 FJM    Fedora core FC4 update.
+  12/06/06 FJM    Removed handler_data from IP.h files - Moved handler_data here
+  01/11/07 FJM    Add support x86_64
+  04/01/09 FJM    blocking_start_convert
+  08/02/09 FJM    Add configuration register access.
 
-{-D}
+  {-D}
 */
 
 
 /*
-    MODULES FUNCTIONAL DETAILS:
+  MODULES FUNCTIONAL DETAILS:
 
-	This file contains the definitions, structures and prototypes for APC8620.
+  This file contains the definitions, structures and prototypes for APC8620.
 */
 
 
@@ -87,8 +87,8 @@ typedef unsigned short word;	/* custom made word data type */
 #undef CSTATUS
 #endif
 typedef int CSTATUS;
-	/*  Custom made CSTATUS data type, used 
-	as return value from the carrier functions. */
+/*  Custom made CSTATUS data type, used 
+    as return value from the carrier functions. */
 
 #define TRUE	1	/* Boolean value for true */
 #define FALSE	0	/* Boolean value for false */
@@ -99,21 +99,36 @@ typedef int CSTATUS;
 #define SLOT_D	0x44	/* Value for slot D */
 #define SLOT_E	0x45	/* Value for slot E */
 
-#define SLOT_A_IO_OFFSET	 0x0180		/*  Slot A IO space addr. offset from carrier base addr. */
-#define SLOT_A_ID_OFFSET	 0x0040		/*  Slot A ID space addr. offset from carrier base addr. */
-#define SLOT_A_MEM_OFFSET	 0x0		/*  Slot A MEM space addr. */
-#define SLOT_B_IO_OFFSET	 0x0200		/*  Slot B IO space addr. offset from carrier base addr. */
-#define SLOT_B_ID_OFFSET	 0x0080		/*  Slot B ID space addr. offset from carrier base addr. */
-#define SLOT_B_MEM_OFFSET	 0x00800000	/*  Slot B MEM space addr. */
-#define SLOT_C_IO_OFFSET	 0x0280		/*  Slot C IO space addr. offset from carrier base addr. */
-#define SLOT_C_ID_OFFSET	 0x00C0		/*  Slot C ID space addr. offset from carrier base addr. */
-#define SLOT_C_MEM_OFFSET	 0x01000000	/*  Slot C MEM space addr. */
-#define SLOT_D_IO_OFFSET	 0x0300		/*  Slot D IO space addr. offset from carrier base addr. */
-#define SLOT_D_ID_OFFSET	 0x0100		/*  Slot D ID space addr. offset from carrier base addr. */
-#define SLOT_D_MEM_OFFSET	 0x01800000	/*  Slot D MEM space addr. */
-#define SLOT_E_IO_OFFSET	 0x0380		/*  Slot E IO space addr. offset from carrier base addr. */
-#define SLOT_E_ID_OFFSET	 0x0140		/*  Slot E ID space addr. offset from carrier base addr. */
-#define SLOT_E_MEM_OFFSET	 0x02000000	/*  Slot E MEM space addr. */
+/*  Slot A IO space addr. offset from carrier base addr. */
+#define SLOT_A_IO_OFFSET	 0x0180
+/*  Slot A ID space addr. offset from carrier base addr. */
+#define SLOT_A_ID_OFFSET	 0x0040
+/*  Slot A MEM space addr. */
+#define SLOT_A_MEM_OFFSET	 0x0
+/*  Slot B IO space addr. offset from carrier base addr. */
+#define SLOT_B_IO_OFFSET	 0x0200
+/*  Slot B ID space addr. offset from carrier base addr. */
+#define SLOT_B_ID_OFFSET	 0x0080
+	/*  Slot B MEM space addr. */
+#define SLOT_B_MEM_OFFSET	 0x00800000
+/*  Slot C IO space addr. offset from carrier base addr. */
+#define SLOT_C_IO_OFFSET	 0x0280
+/*  Slot C ID space addr. offset from carrier base addr. */
+#define SLOT_C_ID_OFFSET	 0x00C0
+/*  Slot C MEM space addr. */
+#define SLOT_C_MEM_OFFSET	 0x01000000
+/*  Slot D IO space addr. offset from carrier base addr. */
+#define SLOT_D_IO_OFFSET	 0x0300
+/*  Slot D ID space addr. offset from carrier base addr. */
+#define SLOT_D_ID_OFFSET	 0x0100
+/*  Slot D MEM space addr. */
+#define SLOT_D_MEM_OFFSET	 0x01800000
+/*  Slot E IO space addr. offset from carrier base addr. */
+#define SLOT_E_IO_OFFSET	 0x0380
+/*  Slot E ID space addr. offset from carrier base addr. */
+#define SLOT_E_ID_OFFSET	 0x0140
+/*  Slot E MEM space addr. */
+#define SLOT_E_MEM_OFFSET	 0x02000000
 
 #define MAX_CARRIERS 4	/* maximum number of carriers */
 #define MAX_SLOTS    5  /* number of IP module slots on this carrier */
@@ -149,9 +164,9 @@ typedef int CSTATUS;
 
 
 /* 
-	CSTATUS return values
-	Errors will have most significant bit set and are preceded with an E_.
-	Success values will be succeeded with an S_.
+   CSTATUS return values
+   Errors will have most significant bit set and are preceded with an E_.
+   Success values will be succeeded with an S_.
 */
 #define ERROR 0x8000 /* general */
 #define E_OUT_OF_MEMORY 	0x8001	/*  Out of memory status value */
@@ -166,79 +181,75 @@ typedef int CSTATUS;
 
 
 /*
-	APC8620	PCI Carrier information
+  APC8620	PCI Carrier information
 */
 #define APC8620_VENDOR_ID 0x10b5	/* PLX vendor ID */
 #define APC8620_DEVICE_ID 0x1024	/* Acromag's device ID */
 
 /* 
-	PCI Carrier data structure
+   PCI Carrier data structure
 */
 
 typedef struct
 {
-	int nHandle;					/* handle of this carrier structure */
-	long lBaseAddress;				/* pointer to base address of carrier board */
-	int nIntLevel;					/* Interrupt level of Carrier */
-	int nInteruptID;				/* ID of interrupt handler */
-	BOOL bInitialized;				/* Carrier initialized flag */
-	BOOL bIntEnabled;				/* interrupts enabled flag */
-	int nCarrierDeviceHandle;		/* handle to an open carrier device */
-	word uCarrierID;				/* Carrier Identification value from open */
-	long lMemBaseAddress;			/* pointer to base address of carrier board memory space */
-	int nDevInstance;				/* Device Instance */
-	char devname[64];				/* device name */
+  int nHandle;		/* handle of this carrier structure */
+  long lBaseAddress;	/* pointer to base address of carrier board */
+  int nIntLevel;	/* Interrupt level of Carrier */
+  int nInteruptID;	/* ID of interrupt handler */
+  BOOL bInitialized;	/* Carrier initialized flag */
+  BOOL bIntEnabled;	/* interrupts enabled flag */
+  int nCarrierDeviceHandle;/* handle to an open carrier device */
+  word uCarrierID;	/* Carrier Identification value from open */
+  long lMemBaseAddress;	/* pointer to base address of carrier board memory space */
+  int nDevInstance;	/* Device Instance */
+  char devname[64];	/* device name */
 }CARRIERDATA_STRUCT;
 
 typedef struct
 {
-	word controlReg;	/*  Status/Control Register */
-	word intPending;	/*  Interrupt Pending Register */
-	word slotAInt0;		/*  Slot A interrupt 0 select space */
-	word slotAInt1;		/*  Slot A interrupt 1 select space */
-	word slotBInt0;		/*  Slot B interrupt 0 select space */
-	word slotBInt1;		/*  Slot B interrupt 1 select space */
-	word slotCInt0;		/*  Slot C interrupt 0 select space */
-	word slotCInt1;		/*  Slot C interrupt 1 select space */
-	word slotDInt0;		/*  Slot D interrupt 0 select space */
-	word slotDInt1;		/*  Slot D interrupt 1 select space */
-	word slotEInt0;		/*  Slot E interrupt 0 select space */
-	word slotEInt1;		/*  Slot E interrupt 1 select space */
-	word IPClockControl;/* IP Clock Control Register */
+  word controlReg;	/*  Status/Control Register */
+  word intPending;	/*  Interrupt Pending Register */
+  word slotAInt0;	/*  Slot A interrupt 0 select space */
+  word slotAInt1;	/*  Slot A interrupt 1 select space */
+  word slotBInt0;	/*  Slot B interrupt 0 select space */
+  word slotBInt1;	/*  Slot B interrupt 1 select space */
+  word slotCInt0;	/*  Slot C interrupt 0 select space */
+  word slotCInt1;	/*  Slot C interrupt 1 select space */
+  word slotDInt0;	/*  Slot D interrupt 0 select space */
+  word slotDInt1;	/*  Slot D interrupt 1 select space */
+  word slotEInt0;	/*  Slot E interrupt 0 select space */
+  word slotEInt1;	/*  Slot E interrupt 1 select space */
+  word IPClockControl;/* IP Clock Control Register */
 }PCI_BOARD_MEMORY_MAP;
 
 
 /*
-    Defined below is the Interrupt Handler Data Structure.
-    The interrupt handler is provided with a pointer that points
-    to this structure.  From this the handler has a link back to
-    its related process and common data area.  The usage of the members
-    is not absolute.  Their typical uses are described below.
+  Defined below is the Interrupt Handler Data Structure.
+  The interrupt handler is provided with a pointer that points
+  to this structure.  From this the handler has a link back to
+  its related process and common data area.  The usage of the members
+  is not absolute.  Their typical uses are described below.
 */
 struct handler_data
-    {
-    int h_pid;          /* Handler related process i.d. number.
-                           Needed to know if handler is going to wake up,
-                           put to sleep, or suspend a process.*/
-    char *hd_ptr;       /* Handler related data pointer.
-                           Needed to know if handler is going to access
-                           a process' data area. */
-    };
+{
+  int h_pid;          /* Handler related process i.d. number.
+			 Needed to know if handler is going to wake up,
+			 put to sleep, or suspend a process.*/
+  char *hd_ptr;       /* Handler related data pointer.
+			 Needed to know if handler is going to access
+			 a process' data area. */
+};
 
 
 /* Interrupt service routine data structure */
 struct isr_data					/* can be enlarged as required */
 {
-	unsigned long slot_io_address;	/* this slots I/O address */
-	unsigned long slot_mem_address;	/* this slots mem address */
+  unsigned long slot_io_address;	/* this slots I/O address */
+  unsigned long slot_mem_address;	/* this slots mem address */
 };
 
-
-
-
-
 /*
-	Function Prototypes
+  Function Prototypes
 */
 CSTATUS GetCarrierAddress(int nHandle, long* pAddress);
 CSTATUS SetCarrierAddress(int nHandle, long lAddress);
@@ -249,7 +260,7 @@ CSTATUS DisableInterrupts(int nHandle);
 CSTATUS SetInterruptLevel(int nHandle, word uLevel);
 CSTATUS GetInterruptLevel(int nHandle, word* pLevel);
 CSTATUS SetInterruptHandler(int nHandle, char chSlot, int nRequestNumber,
-							int nVector, void(*pHandler)(void*), void* pData);
+			    int nVector, void(*pHandler)(void*), void* pData);
 CSTATUS InitCarrierLib(void);
 CSTATUS CarrierOpen(int nDevInstance, int* pHandle);
 CSTATUS CarrierClose(int nHandle);
